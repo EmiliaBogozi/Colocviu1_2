df9da417690e10c849ca880c5f50b48547ff7b66
emilia.bogozi <emiliabogozi1@gmail.com>
2021-04-14 13:53:26 +0300

    Update Constants.java

--- Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Constants.java
+++ Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Constants.java
@@ -2,11 +2,10 @@ package ro.pub.cs.systems.eim.Colocviu1_2;
 
 public interface Constants {
     final public static String TAG = "activitylifecycle";
-    final public static String CALCUL = "calcul";
     final public static int SECONDARY_ACTIVITY_REQUEST_CODE = 1;
-    final public static String SUM = "suma";
     final public static String INTENT_ACTION = "ACTION";
     final public static String BROADCAST_RECEIVER_EXTRA = "brodcast";
 
-
+    final public static String SUM = "suma";
+    final public static String CALCUL = "calcul";
 }
